3fe3432596d2fa0733b95613ea16b6e3b12c8392
script000kiddies000 <55286658+script000kiddies000@users.noreply.github.com>
2020-04-17 19:50:14 +0700

    add info.py
    informasi manga

---
+++ info.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests, re
+
+s = requests.session()
+web = s.get('https://nhentai.net/g/308973/').text
+html = BeautifulSoup(web,'html.parser')
+elemen = html.find('div',{'id':'info'})
+
+judul = elemen.find('h1').text
+subjudul = elemen.find('h2').text
+
+print(f"{judul}\n{subjudul}")
+
+for section in elemen.findAll('div',{'class':'tag-container field-name'}):
+  print(re.sub('\s+', '',section.findAll(text=True, recursive=False)[0]))
+  for a in section.findAll('a'):
+    print(f"- {a.text}")
+
